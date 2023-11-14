9fc2e1a2e21807d2d3b09a6e1df2e1b82757f79e
Mikhail <mikhail.sokolovskiy.2002@mail.ru>
2023-11-14 20:45:48 +0500

    second task

--- src/lr5/Example2.java
+++ src/lr5/Example2.java
@@ -1,7 +1,34 @@
 package lr5;
 
+import java.util.Scanner;
+
 public class Example2 {
     public static void main(String[] args) {
 
+
+        class MyClass{
+            private char symb1 = 'A';
+            private char symb2 = '.';
+
+            public void result() {
+                char ch1;
+                char ch2;
+                if ((int) symb1 > (int) symb2){
+                    ch1 = symb2;
+                    ch2 = symb1;
+                }
+                else {
+                    ch1 = symb1;
+                    ch2 = symb2;
+                }
+
+                for (int i = (int) ch1; i <= (int) ch2; i++){
+                    System.out.println((char) i);
+                }
+            }
+
+        }
+        MyClass _myClass = new MyClass();
+        _myClass.result();
     }
 }
