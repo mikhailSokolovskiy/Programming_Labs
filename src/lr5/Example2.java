d6adb8e0b1cdd133270b6420759e354e02c6ea53
Mikhail <mikhail.sokolovskiy.2002@mail.ru>
2023-11-14 20:30:37 +0500

    frist task

--- src/lr5/Example2.java
+++ src/lr5/Example2.java
@@ -1,4 +1,29 @@
 package lr5;
 
 public class Example2 {
+    public static void main(String[] args) {
+
+
+        class MyClass {
+            private char symb;
+
+            public void set(char _symb) {
+                symb = _symb;
+            }
+
+            public int charCode() {
+                return (int) symb;
+            }
+
+            public void show() {
+                System.out.println("Символ: " + symb);
+                System.out.println("Код символа: " + charCode());
+            }
+        }
+
+        MyClass _myClass = new MyClass();
+        _myClass.set('.');
+        _myClass.charCode();
+        _myClass.show();
+    }
 }
