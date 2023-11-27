abc49206e10cf61c88d2a969017678bc886e2eae
Mikhail <mikhail.sokolovskiy.2002@mail.ru>
2023-11-27 20:32:36 +0500

    second task

---
+++ src/lr6/Example2.java
@@ -0,0 +1,17 @@
+package lr6;
+
+public class Example2 {
+    public static void main(String[] args) {
+        class MyClass {
+            private static int num = 0;
+
+            public static void SetShow(){
+                System.out.println("Текущее значение статического поля: " + num);
+                num += 1;
+                System.out.println("Увеличенное на еденицу значение статического поля: " + num);
+            }
+        }
+        MyClass _myClass = new MyClass();
+        _myClass.SetShow();
+    }
+}
