84f3442d4af24183aaf70a41cd99cbae0851cfbc
Mikhail <mikhail.sokolovskiy.2002@mail.ru>
2023-11-28 20:17:01 +0500

    fourth task (update)

--- src/lr6/Example4.java
+++ src/lr6/Example4.java
@@ -1,19 +1,18 @@
 package lr6;
 
 public class Example4 {
-    public static void main(String[] args) {
-        class MyClass {
-            public static int dbFactorial(int num) {
-                int res = 1;
-                for (int i = num; i >= 0; i = i - 2) {
-                    if (i == 0 || i == 1)
-                        return res;
-                    else
-                        res *= i;
-                }
+    public static int dbFactorial(int num) {
+        int res = 1;
+        for (int i = num; i >= 0; i = i - 2) {
+            if (i == 0 || i == 1)
                 return res;
-            }
+            else
+                res *= i;
         }
-        System.out.println("Двойной факториал: " + MyClass.dbFactorial(6));
+        return res;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Двойной факториал: " + dbFactorial(6));
     }
 }
