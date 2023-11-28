caa99f9394b79d70520c385c1308bb36d2d43ad7
Mikhail <mikhail.sokolovskiy.2002@mail.ru>
2023-11-28 20:32:42 +0500

    fifth task

--- src/lr6/Example5.java
+++ src/lr6/Example5.java
@@ -1,7 +1,14 @@
 package lr6;
 
 public class Example5 {
+    public static int SummOfSqrd(int num){
+        int res = 0;
+        for (int i = 1; i <= num; i++){
+            res += (int) Math.pow(i, 2);
+        }
+        return res;
+    }
     public static void main(String[] args) {
-
+        System.out.println(SummOfSqrd(3));
     }
 }
